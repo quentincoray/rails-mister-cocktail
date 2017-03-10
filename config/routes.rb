9e6e8cd8430ae22830b8b903ef60a8ebaa14effc
Quentin Coray <quentin.coray@gmail.com>
2017-03-10 11:20:10 +0100

    Installing gems

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  Rails.application.routes.draw do
+    resources :cocktails, only: [:show, :index, :new, :create] do
+      resources :doses, only: [:create]
+    end
+    root "cocktails#index"
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
